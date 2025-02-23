81d6ede5916d3bfcb7329c5688ddb58b742689b1
mallard <owlyowl202@gmail.com>
2025-02-23 11:46:25 +0000

    camera works fine now coz i lifted it from example

--- camera.c
+++ camera.c
@@ -2,8 +2,13 @@
 #include "camera.h"
 
 float rotAngleX = 0.0f;  
+float rotAngleY = 0.0f;  
 float camDist = 20.0f;
-float lastTime = 0.0f;
+float lastTimeMs = 0.0f;
+float time = 0.0f;
+
+
+T3DVec3 camDir = {{0,0,1}};
 
 #define RAD_360 6.28318530718f
 
@@ -21,23 +26,39 @@ void camera_draw() {
 }
 
 void camera_update(_SI_condat *inputs, Camera* camera, T3DViewport* viewport) { 
-    rotAngleX = inputs->x * 0.001f;
-
     T3DVec3 upVector = (T3DVec3){{0,1,0}};
+    
+    double nowMs = (double)get_ticks_us() / 1000.0;
+    float deltaTime = (float)(nowMs - lastTimeMs);
+    lastTimeMs = nowMs;
+    time += deltaTime;
 
-    camera->target.x = camera->pos.x + (camera->target.x-camera->pos.x)*fm_cosf(rotAngleX)-(camera->target.z-camera->pos.z)*fm_sinf(rotAngleX);
-    camera->target.z = camera->pos.z + (camera->target.x-camera->pos.x)*fm_sinf(rotAngleX)+(camera->target.z-camera->pos.z)*fm_cosf(rotAngleX);
+    float camSpeed = deltaTime * 0.001f;
+    float camRotSpeed = deltaTime * 0.00001f;
 
-    T3DVec3 translationVec;
-    T3DVec3 normalizedTarget = camera->target;
+    camDir.v[0] = fm_cosf(rotAngleX) * fm_cosf(rotAngleY);
+    camDir.v[1] = fm_sinf(rotAngleY);
+    camDir.v[2] = fm_sinf(rotAngleX) * fm_cosf(rotAngleY);
+    t3d_vec3_norm(&camDir);
 
-    t3d_vec3_mul(&translationVec, &(T3DVec3){{inputs->y * 0.1f,0,inputs->y * 0.1f}}, &camera->target);
+    if(inputs->Z) {
+      rotAngleX += (float)inputs->x * camRotSpeed;
+      rotAngleY += (float)inputs->y * camRotSpeed;
+    } else {
+      camera->pos.v[0] += camDir.v[0] * (float)inputs->y * camSpeed;
+      camera->pos.v[1] += camDir.v[1] * (float)inputs->y * camSpeed;
+      camera->pos.v[2] += camDir.v[2] * (float)inputs->y * camSpeed;
 
-    t3d_vec3_norm(&translationVec); 
-    t3d_vec3_scale(&translationVec, &translationVec, 4.0f);
+      camera->pos.v[0] += camDir.v[2] * (float)inputs->x * -camSpeed;
+      camera->pos.v[2] -= camDir.v[0] * (float)inputs->x * -camSpeed;
+    }
 
-    t3d_vec3_add(&camera->pos, &camera->pos, &translationVec);
-    t3d_vec3_add(&camera->target, &camera->target, &translationVec);
+    if(inputs->C_up)camera->pos.v[1] += camSpeed * 15.0f;
+    if(inputs->C_down)camera->pos.v[1] -= camSpeed * 15.0f;
+
+    camera->target.v[0] = camera->pos.v[0] + camDir.v[0];
+    camera->target.v[1] = camera->pos.v[1] + camDir.v[1];
+    camera->target.v[2] = camera->pos.v[2] + camDir.v[2];
 
     t3d_viewport_set_projection(viewport, T3D_DEG_TO_RAD(85.0f), 1.0f, 100.0f);
     t3d_viewport_look_at(viewport, &camera->pos, &camera->target, &upVector);
