47f795db865b5ae4da07e6c9c1f0c2bd3be54d95
CamashRed <114523140+CamashRed@users.noreply.github.com>
2025-04-08 18:16:47 +0100

    Added setvars for Cam (#5)

---
+++ setvars-cam.sh
@@ -0,0 +1,6 @@
+#export T3D_INST=/mnt/o/src/n64/tiny3d
+export T3D_INST=/opt/tiny3d/
+#export N64_INST=/opt/libdragon/
+export  N64_INST=/opt/libdragon/
+#export ARES_DIR=/mnt/o/src/n64/ares-v141/
+export ARES_DIR=/mnt/e/N64_Game_Stuff/ares-windows/ares-v141/
