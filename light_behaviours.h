e97121f57b32f97a57c75eecec5cabf653e391e0
mallard <owlyowl202@gmail.com>
2025-04-15 12:50:51 +0100

    sensible names for light colours

--- light_behaviours.h
+++ light_behaviours.h
@@ -12,10 +12,10 @@
 /** @section Light Behaviours
  * fuel for light->lightUpdateFunction
 */
-static uint8_t light_col1[4] = {0xFF, 0x00, 0x00, 0xFF};
-static uint8_t light_col2[4] = {0x00, 0xFF, 0x00, 0xFF};
-static uint8_t light_col3[4] = {0x00, 0x00, 0xFF, 0xFF};
-static uint8_t* colourWheel[3] = {light_col1, light_col2, light_col3};
+static uint8_t redClr[4] = {0xFF, 0x00, 0x00, 0xFF};
+static uint8_t greenClr[4] = {0x00, 0xFF, 0x00, 0xFF};
+static uint8_t blueClr[4] = {0x00, 0x00, 0xFF, 0xFF};
+static uint8_t* colourWheel[3] = {redClr, greenClr, blueClr};
 
 /**
  * @brief efficient traffic light colour cycle that uses obj time
@@ -42,7 +42,7 @@ static void light_update_traffic_light_xm(Light *light, float deltaTime, float o
     float secs;
     xm64player_tell(&xm, &patIdx, &row, &secs);
 
-    if (row % 0x04 == 0 && row != _lastRow) {
+    if ((row + 1) % 0x04 == 0 && row != _lastRow) {
         memcpy(light->colour, colourWheel[xmLightIndex++], sizeof(uint8_t[4]));
         if (xmLightIndex == 3) xmLightIndex = 0;
         _lastRow = row;
